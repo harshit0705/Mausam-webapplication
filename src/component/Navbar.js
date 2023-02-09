4675940790d50c5341c6141f0d7109f3adcf1a29
harshit0705 <harshitpandey7987@gmail.com>
2023-02-09 11:19:29 +0530

    2

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -20,7 +20,7 @@ export default function Navbar(props) {
     <div>
       <div className={Nav.main_container}>
         <div className={Nav.name}>
-          <img src="logo123.png" alt="logo" />
+          <img src="icon_rain2.png" alt="logo" />
         </div>
         <div className="input-group mb-3 vh-30" id={Nav.boot}>
           <input
